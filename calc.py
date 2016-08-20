a41a069a6477a67e6d1d9e1916e1e35718235b75
val-sytch <val.sytch@gmail.com>
2016-08-20 19:51:46 +0300

    calc v.2.0 reorganized. static content and some style added. Added data verification (in both client-server sides)

--- calc.py
+++ calc.py
@@ -5,6 +5,7 @@ from decimal import Decimal
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def start_page():
     return render_template("index.html")
@@ -12,25 +13,13 @@ def start_page():
 
 @app.route('/result', methods=['GET', 'POST'])
 def result():
-    def add(first_number, second_number):
-        return first_number + second_number
-
-    def minus(first_number, second_number):
-        return first_number - second_number
-
-    def multiply(first_number, second_number):
-        return first_number * second_number
-
-    def divide(first_number, second_number):
-        return first_number / second_number
     
     operations = {
-        "+": add,
-        "-": minus,
-        "*": multiply,
-        "/": divide
+        "+": lambda first_number, second_number: first_number + second_number,
+        "-": lambda first_number, second_number: first_number - second_number,
+        "*": lambda first_number, second_number: first_number * second_number,
+        "/": lambda first_number, second_number: first_number / second_number,
     }
-
     form = request.form
     first_number = form['first_number']
     operator = form['operator']
@@ -38,8 +27,13 @@ def result():
 
     try:
         result = operations[operator](Decimal(first_number), Decimal(second_number))
-        result_printed = "{0} {1} {2} = {3}".format(first_number, operator,\
-                                                    second_number, result)
+        return render_template('result.html', form = form, result = result)
+    except ArithmeticError:
+       error = "InvalidDataNotNumberError"
+       return render_template('result.html', error = error)
+    except ZeroDivisionError:
+        error = "DivisionByZeroError"
+        return render_template('result.html', error = error)
     except Exception:
-        result_printed = "Oops. Something went wrong. Please try again"
-    return render_template('result-page.html', result = result_printed)
+        error = "UnexpectedAndUnknownError"
+        return render_template('result.html', error = error)
